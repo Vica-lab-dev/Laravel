a1a0540c64fe2a1333af1b829483b25a69ca4a28
Vica-lab-dev <vicomaletkovic@gmail.com>
2025-12-06 04:17:41 +0100

    File rename to WeatherSeeder

---
+++ WeatherProject/database/seeders/WeatherSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CitiesModel;
+use App\Models\WeatherModel;
+use App\Models\ForecastsModel;
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class WeatherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $cities = CitiesModel::all();
+
+        foreach ($cities as $city)
+        {
+            $userWeather = WeatherModel::where('city_id', $city->id)->first();
+            if ($userWeather !== null)
+            {
+                $this->command->error("This city already exists");
+                continue;
+            }
+
+            WeatherModel::create([
+                'city_id' => $city->id,
+                "temperature" => rand(15, 30)
+            ]);
+        }
+    }
+}
