0277dc19516608b98f1edd97efa090ecfed563eb
Vica-lab-dev <vicomaletkovic@gmail.com>
2025-12-15 22:40:50 +0100

    Improve ForecastHelper weather icon logic

---
+++ WeatherProject/app/Http/ForecastHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http;
+
+class ForecastHelper
+{
+
+    const WEATHER_ICONS =
+        [
+            "rainy" => "fa-cloud-rain",
+            "snowy" => "fa-snowflake",
+            "sunny" => "fa-sun"
+        ];
+    public static function getIconByWeatherType($type)
+    {
+        $icon = self::WEATHER_ICONS[$type];
+
+        return $icon;
+
+    }
+    public static function getColorByTemperature($temperature)
+    {
+        if($temperature <= 0)
+        {
+            $color = 'lightblue';
+        }
+        else if($temperature >= 1 && $temperature <= 15)
+        {
+            $color = 'blue';
+        }
+        else if($temperature > 15 && $temperature <= 25)
+        {
+            $color = 'green';
+        }
+        else
+        {
+            $color = 'red';
+        }
+
+        return $color;
+    }
+
+}
