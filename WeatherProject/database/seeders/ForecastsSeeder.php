003bdce0fdf7f3e1ec3e39d95c9d3d9710f3d8b4
Vica-lab-dev <vicomaletkovic@gmail.com>
2025-12-16 00:09:23 +0100

    *Homework 15 - logic in ForecastsSeeder

--- WeatherProject/database/seeders/ForecastsSeeder.php
+++ WeatherProject/database/seeders/ForecastsSeeder.php
@@ -18,9 +18,12 @@ class ForecastsSeeder extends Seeder
         $cities = CitiesModel::all();
         foreach ($cities as $city)
         {
+            $lastTemperature = null;
+
             for($i = 0; $i < 5; $i++)
             {
-                $weatherType = ForecastsModel::WEATHERS[rand(0, 2)];
+
+                $weatherType = ForecastsModel::WEATHERS[rand(0, 3)];
 
                 $probability = null;
 
@@ -28,13 +31,48 @@ class ForecastsSeeder extends Seeder
                 {
                     $probability = rand(1, 100);
                 }
+
+                if($lastTemperature !== null)
+                {
+                    $minTemperature = $lastTemperature - 5;
+                    $maxTemperature = $lastTemperature + 5;
+                    $temperature = rand($minTemperature, $maxTemperature);
+                }
+
+                else
+                {
+
+                    switch($weatherType)
+                    {
+                        case 'rainy':
+                            $temperature = rand(-30, 50);
+                            break;
+
+                        case 'cloudy':
+                            $temperature = rand(-30, 15);
+                            break;
+                        case "rainy":
+                            $temperature = rand(-10, 50);
+                            break;
+                        case "snowy":
+                            $temperature = rand(-30, 1);
+                            break;
+                    }
+                }
+
+
+
+
+
                 ForecastsModel::create([
                     'city_id' => $city->id,
-                    'temperature' => rand(15, 30),
+                    'temperature' => $temperature,
                     'forecast_date' => Carbon::now()->addDays(rand(1, 30)),
                     'weather_type' => $weatherType,
                     'probability' => $probability,
                 ]);
+
+                $lastTemperature = $temperature;
             }
         }
     }
