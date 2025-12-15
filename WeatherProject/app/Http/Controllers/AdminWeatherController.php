54c55d387b85934056035b8fb5abf66e0f035631
Vica-lab-dev <vicomaletkovic@gmail.com>
2025-12-15 18:29:52 +0100

    *Homework 14 - forecastUpdate function in Admin Weather Controller

--- WeatherProject/app/Http/Controllers/AdminWeatherController.php
+++ WeatherProject/app/Http/Controllers/AdminWeatherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ForecastsModel;
 use App\Models\WeatherModel;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,8 @@ class AdminWeatherController extends Controller
 {
     public function update(Request $request)
     {
-       $request->validate([
+       $request->validate
+       ([
            'temperature' => 'required',
            'city_id' => 'required|exists:cities,id',
        ]);
@@ -19,5 +21,29 @@ class AdminWeatherController extends Controller
        $weather->save();
 
        return redirect()->back();
+    }
+
+    public function forecastUpdate(Request $request)
+    {
+        $request->validate
+        ([
+            'city_id' => 'required|exists:cities,id',
+            'temperature' => 'required',
+            'weather_type' => 'required',
+            'probability' => 'numeric|nullable',
+            'forecast_date' => 'required',
+        ]);
+
+        ForecastsModel::create
+        ([
+            'city_id' => $request->get('city_id'),
+            'temperature' => $request->get('temperature'),
+            'weather_type' => $request->get('weather_type'),
+            'probability' => $request->get('probability'),
+            'forecast_date' => $request->get('forecast_date'),
+        ]);
+
+
+        return redirect()->back();
     }
 }
