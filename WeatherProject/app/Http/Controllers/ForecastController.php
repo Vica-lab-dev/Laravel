653276a35f2db63ecb1d997c7ed8b3241ed7acda
Vica-lab-dev <vicomaletkovic@gmail.com>
2025-12-26 21:06:18 +0100

    *Homework 19 - Call test Artisan command from controller

--- WeatherProject/app/Http/Controllers/ForecastController.php
+++ WeatherProject/app/Http/Controllers/ForecastController.php
@@ -6,6 +6,7 @@ use App\Models\CitiesModel;
 use App\Models\ForecastsModel;
 use App\Models\WeatherModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 
 class ForecastController extends Controller
@@ -62,6 +63,8 @@ class ForecastController extends Controller
     public function search(Request $request)
     {
         $cityName = $request->get('city');
+
+        Artisan::call("app:test-command", ['city' => $cityName]);
 
         $cities = CitiesModel::with('todaysForecast')
             ->where('name', 'LIKE', "%{$cityName}%")->get();
