1eb724c93a8dad1672b47a4275936ccfc7e1a54f
Vica-lab-dev <vicomaletkovic@gmail.com>
2025-12-29 20:37:15 +0100

    *Homework 21 - Added Contact Repository

---
+++ Lecture10Middleware/app/Repositories/ContactRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\ContactModel;
+
+class ContactRepository
+{
+    private $contactModel;
+
+    public function __construct()
+    {
+        $this->contactModel = new ContactModel();
+    }
+    public function createNew($request)
+    {
+        $this->contactModel->create([
+            "email" => $request->get("email"),
+            "subject" => $request->get("subject"),
+            "message" => $request->get("description")
+        ]);
+    }
+
+    public function delete($id)
+    {
+        return $this->contactModel->where("id", $id)->first();
+    }
+
+    public function update($request, $contact)
+    {
+        $contact -> email = $request->get("email");
+        $contact -> subject = $request->get("subject");
+        $contact -> message = $request->get("message");
+        $contact->save();
+
+    }
+}
