f8aae773a120fc519a8c8ef0842cc71b6f22d5e4
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-03 20:35:17 +0100

    Implement admin page creation with validation and repository

--- LaravelCertificationProject/app/Http/Controllers/AdminController.php
+++ LaravelCertificationProject/app/Http/Controllers/AdminController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AdminPageRequest;
+use App\Models\pagesModel;
+use App\Repositories\AdminRepository;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    private $pagesRepo;
+
+    public function __construct()
+    {
+        $this->pagesRepo = new AdminRepository();
+    }
     public function page()
     {
         return view('adminPage');
+    }
+
+    public function create(AdminPageRequest $request)
+    {
+        $this->pagesRepo->createNew($request);
+        return redirect()->back();
     }
 }
