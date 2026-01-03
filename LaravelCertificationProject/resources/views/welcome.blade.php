8da2ad430c847c89d93a3d7e60347e88b739c21e
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-03 19:42:55 +0100

    Welcome page

---
+++ LaravelCertificationProject/resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('bootstrap')
+
+
+<div class="card" style="width: 18rem;">
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"><a href="{{ route('admin.page') }}">Admin Page</a></li>
+        <li class="list-group-item"><a href="">Editor Page</a></li>
+    </ul>
+</div>
