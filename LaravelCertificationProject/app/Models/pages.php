c84c0740ae1cb859c69f18113dc4f6dfd3a43db8
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-03 19:40:19 +0100

    Project Started

---
+++ LaravelCertificationProject/app/Models/pages.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class pages extends Model
+{
+    protected $table = 'pages';
+
+    protected $fillable = ['name','description','url'];
+}
