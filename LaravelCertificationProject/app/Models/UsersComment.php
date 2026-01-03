81d34670783c045ce72aaf42a6102b94eb370a00
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-03 23:34:45 +0100

    Added userComment relation to UsersComment model

--- LaravelCertificationProject/app/Models/UsersComment.php
+++ LaravelCertificationProject/app/Models/UsersComment.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use function Illuminate\Support\php_binary;
 
 class UsersComment extends Model
 {
     protected $table = 'users_comments';
     protected $fillable = ['user_id', 'comment'];
+
+    public function userComment()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
