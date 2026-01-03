6091549b0d7ae82468e1699566aa0cbc34c08800
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-03 19:41:56 +0100

    Added Admin Page

---
+++ LaravelCertificationProject/resources/views/adminPage.blade.php
@@ -0,0 +1,17 @@
+@extends('bootstrap')
+
+<form>
+    <div class="mb-3">
+        <label for="PageID" class="form-label">Enter Page name</label>
+        <input type="text" class="form-control" id="PageID" name="page">
+    </div>
+    <div class="mb-3">
+        <label for="descriptionID" class="form-label">Enter page descritpion</label>
+        <input type="text" class="form-control" id="descriptionID" name="description">
+    </div>
+    <div class="mb-3">
+        <label for="urlID" class="form-label">Enter page URL</label>
+        <input type="text" class="form-control" id="urlID" name="url">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
