7fba8715fe2882e8342000bb306e43d81019b889
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-05 20:55:02 +0100

    Implement order checkout and inventory update

--- Lecture10Middleware/app/Http/Controllers/ShoppingCartController.php
+++ Lecture10Middleware/app/Http/Controllers/ShoppingCartController.php
@@ -3,31 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CartAddRequest;
+use App\Models\OrderItems;
+use App\Models\Orders;
 use App\Models\ProductsModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class ShoppingCartController extends Controller
 {
     public function index()
     {
-       //$allProducts = [];
-
-       // foreach (Session::get('product') as $cartItem)
-       // {
-       //     $allProducts[] = $cartItem['product_id'];
-       // }
-       // $products = ProductsModel::whereIn('id', $allProducts)->get();
-
-        //$allProducts = array_column(Session::get('product'), 'product_id');
-       // $products = ProductsModel::whereIn('id', $allProducts)->get();
-
-
-       // return view('cart', [
-       //     'cart' => Session::get('product'),
-        //    'products' => $products,
-        //]);
-
+        $cart = Session::get('product');
+        if(count($cart) < 0)
+        {
+            return redirect('/');
+        }
 
        $combined = [];
        foreach(Session::get('product') as $item)
@@ -50,6 +41,47 @@ class ShoppingCartController extends Controller
            ]);
 
     }
+
+    public function finishOrder()
+    {
+        $cart = Session::get('product');
+        $totalCartPrice = 0;
+
+        foreach($cart as $item)
+        {
+            $product = ProductsModel::firstWhere(['id' => $item['product_id']]);
+            $totalCartPrice += $product->price * $item['amount'];
+            if($item['amount'] > $product->amount )
+            {
+                return redirect()->back();
+            }
+        }
+
+        $order = Orders::create([
+            'user_id' => Auth::id(),
+            'price' => $totalCartPrice,
+        ]);
+
+        foreach($cart as $item)
+        {
+            $product = ProductsModel::firstWhere(['id' => $item['product_id']]);
+            $product->amount -= $item['amount'];
+            $product->save();
+
+            OrderItems::create([
+                'order_id' => $order->id,
+                'product_id' => $product->id,
+                'amount' => $item['amount'],
+                'price' => $item['amount'] * $product->price,
+            ]);
+
+        }
+
+        Session::remove('product');
+
+        return view('ThankYou');
+    }
+
     public function addToCart(CartAddRequest $request)
     {
         $product = ProductsModel::find($request->id);
