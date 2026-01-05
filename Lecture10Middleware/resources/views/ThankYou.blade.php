1d6bd34eafdf5cab98da937a088388bf5466b93f
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-05 20:56:45 +0100

    Added Thank You blade

---
+++ Lecture10Middleware/resources/views/ThankYou.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('PageContent')
+    <div class="container">
+        <p>Your order is successful. It will be delivered within 5 days!</p>
+        <p>Thank you for your trust!</p>
+        <a href="/">Return me to the home page!</a>
+    </div>
+
+@endsection
