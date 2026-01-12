441f1563a469906586a0ef14ff2f229153a45820
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-12 12:05:10 +0100

    Refactor cache callback to arrow function

--- Redis/app/Http/Controllers/HomeController.php
+++ Redis/app/Http/Controllers/HomeController.php
@@ -10,21 +10,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        //$products = [];
-        //if(Cache::has('allProducts'))
-       // {
-       //     $products =Cache::get('allProducts');
-        //}
-        //else
-        //{
-         //   $products = Products::latest()->take(9)->get();
-         //   Cache::put('allProducts', $products, 300);
-        //}
-
-        $products = Cache::remember('allProducts', 300, function()
-        {
-           return Products::latest()->take(9)->get();
-        });
+        $products = Cache::remember('allProducts', 300, fn() => Products::latest()->take(9)->get());
 
         return view('welcome', [
             'products' => $products
