f27464a8e08761a825bf03717eba6226843d6328
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-12 11:55:33 +0100

    Add Products Controller

---
+++ Redis/app/Http/Controllers/ProductsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateProductRequest;
+use App\Models\Products;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class ProductsController extends Controller
+{
+    public function create(CreateProductRequest $request)
+    {
+        Products::create($request->validated());
+
+        return redirect()->back();
+    }
+
+    public function flush()
+    {
+        Cache::forget('allProducts');
+        return redirect()->route('home');
+    }
+}
