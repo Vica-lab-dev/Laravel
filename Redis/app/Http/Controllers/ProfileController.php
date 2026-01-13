4fc5dbbb15c0eef2ea6d2f5b13fc4d2142d64446
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-13 21:58:59 +0100

    Update user avatar and remove old image

---
+++ Redis/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\NewAvatarRequest;
+use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display the user's profile form.
+     */
+    public function edit(Request $request): View
+    {
+        return view('profile.edit', [
+            'user' => $request->user(),
+        ]);
+    }
+
+    /**
+     * Update the user's profile information.
+     */
+    public function update(ProfileUpdateRequest $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+    }
+
+    public function changeAvatar(NewAvatarRequest $request)
+    {
+        $filePath = $request->file('profile_image')
+        ->store('images/avatars', 'public');
+
+        $name = basename($filePath);
+
+        $avatar = Auth::user()->avatar;
+
+        if($avatar !== null)
+        {
+            File::delete("storage/images/avatars/$avatar");
+        }
+
+        Auth::user()->update(['avatar' => $name]);
+
+        return redirect()->back();
+
+    }
+
+
+    /**
+     * Delete the user's account.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        $request->validateWithBag('userDeletion', [
+            'password' => ['required', 'current_password'],
+        ]);
+
+        $user = $request->user();
+
+        Auth::logout();
+
+        $user->delete();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return Redirect::to('/');
+    }
+}
