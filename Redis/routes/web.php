e201b4bf4a592804242b2c5bf3718d0f5f1252d0
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-13 22:00:09 +0100

    Add new routes

--- Redis/routes/web.php
+++ Redis/routes/web.php
@@ -2,11 +2,26 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductsController;
-use Illuminate\Support\Facades\Cache;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/profile/change-avatar', [ProfileController::class, 'changeAvatar'])->name('profile.changeAvatar');
+});
+
+require __DIR__.'/auth.php';
 Route::view('/products', 'create_product');
 Route::post('/products/create', [ProductsController::class, 'create']);
 Route::get("/products/flush", [ProductsController::class, 'flush']);
