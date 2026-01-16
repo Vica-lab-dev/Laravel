96e63adec2d642ed16fca9dc6284a20040698c87
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-16 22:00:24 +0100

    Add role constants and validate role assignment

--- TruckProject/app/Models/User.php
+++ TruckProject/app/Models/User.php
@@ -12,37 +12,45 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    const ROLE_CLIENT = 'client';
+
+    const ROLE_ADMINISTRATOR = 'administrator';
+
+    const ROLE_TRUCKER = 'trucker';
+
+    const ALLOWED_ROLES = [
+        self::ROLE_CLIENT, self::ROLE_ADMINISTRATOR, self::ROLE_TRUCKER
+    ];
+
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function setRoleAttribute(string $role)
+    {
+        if(!in_array($role, self::ALLOWED_ROLES))
+        {
+            throw new \Exception('Invalid role');
+        }
+
+        $this->attributes['role'] = $role;
     }
 }
