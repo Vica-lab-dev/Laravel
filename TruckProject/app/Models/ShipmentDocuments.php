94654f3df28220a1c132996b389328d408ad1115
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-16 03:04:49 +0100

    Add shipment documents model and migration

---
+++ TruckProject/app/Models/ShipmentDocuments.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShipmentDocuments extends Model
+{
+    protected $table = 'shipment_documents';
+
+    protected $fillable = ['shipment_id', 'document_name'];
+}
