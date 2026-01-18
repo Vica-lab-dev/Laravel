cba7e031e3a03796463131d7f9f75321a2a36c2d
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-18 15:55:47 +0100

    Homework 30 - Correct ShipmentPolicy logic

--- TruckProject/app/Policies/ShipmentPolicy.php
+++ TruckProject/app/Policies/ShipmentPolicy.php
@@ -20,9 +20,9 @@ class ShipmentPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user): bool
+    public function view(User $user, Shipment $shipment): bool
     {
-        return $user->role === User::ROLE_ADMINISTRATOR;
+        return false;
     }
 
     /**
@@ -63,5 +63,10 @@ class ShipmentPolicy
     public function forceDelete(User $user, Shipment $shipment): bool
     {
         return false;
+    }
+
+    public function canViewCreationPage(User $user): bool
+    {
+        return $user->role === User::ROLE_ADMINISTRATOR;
     }
 }
