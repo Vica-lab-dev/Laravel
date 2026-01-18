c4441adcc6b0d12bcc63ac1739ddcf9c1bcf11d6
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-18 16:47:31 +0100

    Explicitly define shipment route parameter

--- TruckProject/routes/web.php
+++ TruckProject/routes/web.php
@@ -26,4 +26,5 @@ Route::middleware('auth')->group(function () {
 require __DIR__ . '/auth.php';
 
 
-Route::resource('shipments', ShipmentController::class);
+Route::resource('shipments', ShipmentController::class)
+->parameters(['shipments' => 'shipment']);
