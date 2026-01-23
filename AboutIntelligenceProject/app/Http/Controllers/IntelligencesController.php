66e96579a2c3cc14fd600e928da8f9a4738ae90f
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-23 14:15:37 +0100

    Refactor information creation flow with redirect and relations

--- AboutIntelligenceProject/app/Http/Controllers/IntelligencesController.php
+++ AboutIntelligenceProject/app/Http/Controllers/IntelligencesController.php
@@ -12,21 +12,44 @@ class IntelligencesController extends Controller
 {
     public function createInfo(informationRequest $request)
     {
-        informationModel::create([
+        $information = informationModel::create([
             'name' => $request->name,
             'email' => $request->email,
             'user_id' => $request->user_id,
             'country' => $request->country,
         ]);
 
-        return view('categoriesForm');
+        return redirect()->route('information.show', $information->id);
+
     }
 
     public function categoryCreate(CategoryRequest $request)
     {
-        categoryModel::create([
+        $catInfo = categoryModel::create([
+            'information_id' => $request->information_id,
             'category' => $request->category,
             'percent' => $request->percent,
         ]);
+
+        return redirect()->route('related.show', $catInfo->id);
+
+    }
+
+    public function show(informationModel $information)
+    {
+        return view('information', compact('information'));
+    }
+
+    public function allInformation()
+    {
+        $information = informationModel::all();
+        return view('categoriesForm', compact('information'));
+    }
+
+    public function showRelated()
+    {
+        $information = informationModel::all();
+
+        return view('related', compact('information'));
     }
 }
