c20d7c7d22aca2ba96921ee855f1ac800b425a0e
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-23 01:22:32 +0100

    Add createInfo method to save intelligence information

---
+++ AboutIntelligenceProject/app/Http/Controllers/IntelligencesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\informationRequest;
+use App\Models\informationModel;
+use Illuminate\Http\Request;
+
+class IntelligencesController extends Controller
+{
+    public function createInfo(informationRequest $request)
+    {
+        informationModel::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'user_id' => $request->user_id,
+            'country' => $request->country,
+        ]);
+    }
+}
