d9abbce0c617390c5acff0f04e9020be8b5af80f
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-23 01:50:31 +0100

    Add CategoryRequest

---
+++ AboutIntelligenceProject/app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'category' => 'required',
+            'percent' => 'required|integer|min:0|max:100',
+        ];
+    }
+}
