60b6a1a7f4f93f0cbc46a062c70599051ef880df
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-23 15:47:34 +0100

    Add labels to display user information clearly

--- AboutIntelligenceProject/resources/views/information.blade.php
+++ AboutIntelligenceProject/resources/views/information.blade.php
@@ -1,6 +1,6 @@
 @extends('cdn')
 
-<p>{{ $information->name }}</p>
-<p>{{ $information->email }}</p>
-<p>{{ $information->country }}</p>
+<p>Your name: {{ $information->name }}</p>
+<p>Your Email: {{ $information->email }}</p>
+<p>Your country: {{ $information->country }}</p>
 <a class="btn btn-primary" href="{{ route('allInformation.show') }}">Let's create intelligence category</a>
