18c88dbe80cc092b563de6191f2758efcb2afe39
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-23 01:50:12 +0100

    Change table name from category to categories in categoryModel

--- AboutIntelligenceProject/app/Models/categoryModel.php
+++ AboutIntelligenceProject/app/Models/categoryModel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class categoryModel extends Model
 {
-    protected $table = 'category';
+    protected $table = 'categories';
 
     protected $fillable = ['category', 'percent'];
 }
