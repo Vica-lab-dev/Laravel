1847e7e37a9fbc9f7e3ab3beba26c9b8e4547429
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-23 14:16:04 +0100

    Add hasMany relation to informationModel

--- AboutIntelligenceProject/app/Models/informationModel.php
+++ AboutIntelligenceProject/app/Models/informationModel.php
@@ -9,4 +9,9 @@ class informationModel extends Model
     protected $table = 'information';
 
     protected $fillable = ['user_id', 'name','email','country'];
+
+    public function allInformation()
+    {
+        return $this->hasMany(categoryModel::class, 'information_id', 'id');
+    }
 }
