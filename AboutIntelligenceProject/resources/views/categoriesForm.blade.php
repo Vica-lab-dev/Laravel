2a7fdd62a3a5c9b413e1ee997211ea20ac6cec46
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-23 14:18:50 +0100

    Use foreach to display multiple information records in categoriesForm

--- AboutIntelligenceProject/resources/views/categoriesForm.blade.php
+++ AboutIntelligenceProject/resources/views/categoriesForm.blade.php
@@ -3,16 +3,20 @@
 
         @csrf
 
+        @foreach($information as $info)
+            <input type="hidden" value="{{ $info->id }}" name="information_id">
+        @endforeach
+
         <select class="form-select" aria-label="Default select example" name="category" required>
             <option selected disabled>Choose your intelligence category</option>
-            <option value="1">Linguistic intelligence</option>
-            <option value="2">Logical-mathematical intelligence</option>
-            <option value="3">Visual-spatial intelligence</option>
-            <option value="4">Musical intelligence</option>
-            <option value="5">Bodily-kinesthetic intelligence</option>
-            <option value="6">Interpersonal intelligence</option>
-            <option value="7">Intrapersonal intelligence</option>
-            <option value="8">Naturalistic intelligence</option>
+            <option value="Linguistic intelligence">Linguistic intelligence</option>
+            <option value="Logical-mathematical intelligence">Logical-mathematical intelligence</option>
+            <option value="Visual-spatial intelligence">Visual-spatial intelligence</option>
+            <option value="Musical intelligence">Musical intelligence</option>
+            <option value="Bodily-kinesthetic intelligence">Bodily-kinesthetic intelligence</option>
+            <option value="Interpersonal intelligence">Interpersonal intelligence</option>
+            <option value="Intrapersonal intelligence">Intrapersonal intelligence</option>
+            <option value="Naturalistic intelligence">Naturalistic intelligence</option>
         </select>
 
         <div class="mb-3">
