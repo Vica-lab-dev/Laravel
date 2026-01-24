c2c534bc4de0513fd51c8c30681e3a1756c698b7
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-24 16:22:57 +0100

    User information with nested cards and call-to-action button

--- AboutIntelligenceProject/resources/views/related.blade.php
+++ AboutIntelligenceProject/resources/views/related.blade.php
@@ -1,16 +1,25 @@
 @extends('cdn')
 
-@foreach($information as $info)
-    @foreach($info->allInformation as $singleInfo)
+@section('content')
+    <div class="container d-flex flex-column align-items-center justify-content-center" style="min-height: 80vh;">
+        <div class="text-center">
+            @foreach($information as $info)
+                @foreach($info->allInformation as $singleInfo)
+                    <div class="card mb-3 shadow-sm" style="max-width: 400px;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $info->name }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $info->country }}</h6>
+                            <p class="card-text">
+                                Category: {{ $singleInfo->category }} <br>
+                                Percent: {{ $singleInfo->percent }}%
+                            </p>
+                        </div>
+                    </div>
+                @endforeach
+            @endforeach
 
-            <p>
-                {{$info->name}},
-                {{$info->country}},
-                {{$singleInfo->category}}
-                {{$singleInfo->percent}}%
-            </p>
+            <a href="/interests" class="btn btn-primary mt-3">Are you curious? Click!</a>
+        </div>
+    </div>
+@endsection
 
-    @endforeach
-@endforeach
-
-<a href="/interests" class="btn btn-primary">Are you curious? Click!</a>
