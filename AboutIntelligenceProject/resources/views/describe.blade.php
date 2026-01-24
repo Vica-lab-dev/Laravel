f0f11f98752ab10f708f268818b52054c528b4bc
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-24 15:48:40 +0100

    Add <a> Tag to desribe view

--- AboutIntelligenceProject/resources/views/describe.blade.php
+++ AboutIntelligenceProject/resources/views/describe.blade.php
@@ -1,4 +1,6 @@
+@extends('cdn')
 @foreach($selected as $select => $describe)
     <p>{{$select}}: {{$describe}}</p>
 
 @endforeach
+<a href="{{ route('quiz') }}" class="btn btn-primary">Take the Quiz</a>
