fe9f52d2435aebc7f614649ba5340f840dce55f9
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-24 16:40:07 +0100

    Add view to show selected intelligence interests with descriptions and button to start quiz

--- AboutIntelligenceProject/resources/views/describe.blade.php
+++ AboutIntelligenceProject/resources/views/describe.blade.php
@@ -1,6 +1,17 @@
 @extends('cdn')
-@foreach($selected as $select => $describe)
-    <p>{{$select}}: {{$describe}}</p>
 
-@endforeach
-<a href="{{ route('quiz') }}" class="btn btn-primary">Take the Quiz</a>
+@section('content')
+    <div class="container d-flex justify-content-center align-items-center" style="min-height: 80vh;">
+        <div class="card shadow p-4" style="max-width: 500px; width: 100%;">
+            <h3 class="card-title text-center mb-4">Your Interests Description</h3>
+
+            @foreach($selected as $select => $describe)
+                <p><strong>{{ $select }}:</strong> {{ $describe }}</p>
+            @endforeach
+
+            <div class="text-center mt-3">
+                <a href="{{ route('quiz') }}" class="btn btn-primary">Take the Quiz</a>
+            </div>
+        </div>
+    </div>
+@endsection
