25e299e722815ba689033437bd35ab42bad76560
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-31 18:05:24 +0100

    Add BookRequest with validation rules for creating a book

---
+++ Online-Library/app/Http/Requests/BookRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:1|max:64',
+            'author' => 'required|string|min:1|max:64',
+            'description' => 'required|string|min:1|max:248',
+            'price' => 'required|numeric|min:1',
+        ];
+    }
+}
