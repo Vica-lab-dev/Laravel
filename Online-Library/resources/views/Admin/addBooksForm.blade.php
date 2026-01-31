9e078612df060f89c49e6d9db2bf45e3b0e51238
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-31 21:32:28 +0100

    Refactor addBooksForm page to use bootstraplink.blade and set title

--- Online-Library/resources/views/Admin/addBooksForm.blade.php
+++ Online-Library/resources/views/Admin/addBooksForm.blade.php
@@ -1,9 +1,7 @@
-@extends('layout')
+@extends('bootstraplink')
+<title>Add-Book</title>
 
-@section('title', 'Add Book')
-
-@section('content')
-    <div class="container mt-5">
+<div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card shadow-sm">
@@ -48,4 +46,4 @@
             </div>
         </div>
     </div>
-@endsection
+
