39b13d6e5029a622f53d8e80c77daa260b58b8c1
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-01 10:09:17 +0100

    Separate book search and order logic in UserController

--- Online-Library/app/Http/Controllers/UserController.php
+++ Online-Library/app/Http/Controllers/UserController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserOrderRequest;
 use App\Models\BookModel;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
-    public function search(Request $request, BookModel $book)
+    public function book(BookModel $book)
+    {
+        return view('search', compact( 'book'));
+    }
+
+    public function search(Request $request)
     {
         $name = $request->get('search');
-        $foundBook = BookModel::where('name', 'LIKE', "%{$name}%")
+        $book = BookModel::where('name', 'LIKE', "%{$name}%")
             ->orWhere('author', 'LIKE', "%{$name}%")->first();
-        return view('search', compact('foundBook', 'book'));
+        return view('search', compact( 'book'));
+    }
 
+    public function sessionOrder(BookModel $book)
+    {
+        return view('sessionOrder', compact( 'book'));
     }
 }
