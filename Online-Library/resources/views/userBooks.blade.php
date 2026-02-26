6635ac7718329d5f7af6681edb9b56d4805bb2fc
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-26 23:54:44 +0100

    fix(orders): handle deleted books in user orders list

--- Online-Library/resources/views/userBooks.blade.php
+++ Online-Library/resources/views/userBooks.blade.php
@@ -3,12 +3,13 @@
 <div class="container mt-5">
     <div class="row g-4">
 
-        @foreach($items as $item)
+        @forelse($items as $item)
+            @if($item->book)
             <div class="col-md-3 mb-5">
                 <div class="card h-100 shadow-sm align-items-center">
                     <div class="card-body d-flex flex-column text-center">
                         <div>
-                            <img style="width: 200px;" src="{{asset('/storage/images/books/'. $item->book->image)}}" alt="BookImage">
+                            <img style="width: 200px;" src="{{asset('/storage/images/books/'. $item->book->image)}} " alt="BookImage">
                         </div>
                         <h5 class="card-title">{{ $item->book->name }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{{ $item->book->author }}</h6>
@@ -17,7 +18,12 @@
                     </div>
                 </div>
             </div>
-        @endforeach
+            @endif
+        @empty
+            <div class="col-12 d-flex justify-content-center align-items-center" style="height: 300px;">
+                <p class="fw-bold fs-4 text-center text-muted">You do not have any ordered book!</p>
+            </div>
+        @endforelse
 
     </div>
 </div>
