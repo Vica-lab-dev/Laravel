539ccc63a3d45d0cc1441751339f64594346d84e
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-03-28 21:31:34 +0100

    Add order status constants and allowed statuses list to OrderModel

--- Online-Library/app/Models/OrderModel.php
+++ Online-Library/app/Models/OrderModel.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderModel extends Model
 {
+    const STATUS_PAID = "paid";
+    const STATUS_UNPAID = "unpaid";
+    const STATUS_CANCELED = "canceled";
+
+    const ALLOWED_STATUSES = [
+        self::STATUS_PAID,
+        self::STATUS_UNPAID,
+        self::STATUS_CANCELED
+    ];
+
     protected $table = 'orders';
 
     protected $fillable = ['user_id','status', 'price' ];
