f1c63edddcda9f94b7f81557e5304fadd3918737
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-09-02 13:02:53 +0200

    feat: add provider relationship to User model

--- ServiceHub/app/Models/User.php
+++ ServiceHub/app/Models/User.php
@@ -9,6 +9,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -32,5 +33,10 @@ class User extends Authenticatable
             'role' => UserRole::class,
             'status' => UserStatus::class,
         ];
+    }
+
+    public function provider(): HasOne
+    {
+        return $this->hasOne(related: Provider::class);
     }
 }
