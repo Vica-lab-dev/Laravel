43ad9b9534d8ad9385eb76ef945533ef87ad1b69
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-09-03 12:02:56 +0200

    feat: add bookings relationship to User model

--- ServiceHub/app/Models/User.php
+++ ServiceHub/app/Models/User.php
@@ -9,6 +9,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -38,5 +39,10 @@ class User extends Authenticatable
     public function provider(): HasOne
     {
         return $this->hasOne(related: Provider::class);
+    }
+
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(related: Booking::class);
     }
 }
