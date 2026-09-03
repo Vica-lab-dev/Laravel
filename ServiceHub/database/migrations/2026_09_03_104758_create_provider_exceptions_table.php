8e511e1aefd3d61799ca27f925f3be27003d30b3
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-09-03 12:51:49 +0200

    refactor: add provider_exceptions migration

---
+++ ServiceHub/database/migrations/2026_09_03_104758_create_provider_exceptions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(table: 'provider_exceptions', callback: function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid(column: 'provider_id')->constrained('providers')->cascadeOnDelete();
+            $table->date(column: 'exception_date');
+            $table->time(column: 'start_time')->nullable();
+            $table->time(column: 'end_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(table: 'provider_exceptions');
+    }
+};
