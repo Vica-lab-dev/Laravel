2b6140398b86a7a59197809f3be1d5b9c8bd53bb
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-09-03 11:53:36 +0200

    feat: add bookings migration

---
+++ ServiceHub/database/migrations/2026_09_03_094105_create_bookings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(table: 'bookings', callback: function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(column: 'user_id')->constrained(table: 'users')->cascadeOnDelete();
+            $table->foreignId(column: 'service_id')->constrained(table: 'services')->cascadeOnDelete();
+            $table->foreignUuid(column: 'provider_id')->constrained(table: 'providers')->cascadeOnDelete();
+            $table->dateTime(column: 'starts_at');
+            $table->dateTime(column: 'ends_at');
+            $table->integer(column: 'price');
+            $table->string(column: 'status')->default(value: 'pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(table: 'bookings');
+    }
+};
