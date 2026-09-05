be63b85b56a3be23eae2a80f41d46b649fb3bf6d
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-09-05 11:34:31 +0200

    feat: add HasFactory trait to WorkingHour model

--- ServiceHub/app/Models/WorkingHour.php
+++ ServiceHub/app/Models/WorkingHour.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Enums\Working_Hours\WorkingDay;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -14,6 +15,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 ])]
 class WorkingHour extends Model
 {
+    use HasFactory;
     protected function casts(): array
     {
         return [
