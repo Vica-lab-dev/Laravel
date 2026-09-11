db74ff08b665f49729595927ad028df690fbf389
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-09-11 15:11:42 +0200

    feat: seed users, providers, services, bookings and reviews

--- ServiceHub/database/seeders/DatabaseSeeder.php
+++ ServiceHub/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Booking;
+use App\Models\Category;
+use App\Models\Provider;
+use App\Models\ProviderException;
+use App\Models\Review;
+use App\Models\Service;
 use App\Models\User;
+use App\Models\WorkingHour;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +22,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $customers = User::factory()->count(count: 10)->create();
+        User::factory()->count(count: 10)->admin()->create();
+        $services = collect();
+        $providers = Provider::factory()->count(count: 10)->create();
+        $categories = Category::factory()->count(count: 10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
+        foreach ($providers as $provider) {
+            WorkingHour::factory()->count(count: 5)->forProvider($provider)->create();
+            ProviderException::factory()->count(count: 2)->forProvider($provider)->create();
+            $providerServices = Service::factory()->count(count: 10)->forProvider($provider)->create();
+
+            $services = $services->merge($providerServices);
+        }
+
+        foreach ($services as $service) {
+            $categoriesIds = $categories->random(rand(1, 3))->pluck(value: 'id')->toArray();
+
+            $service->categories()->sync($categoriesIds);
+        }
+
+        $customer = $customers->random();
+        $service = $services->random();
+
+        $booking = Booking::factory()->forUser($customer)->forService($service)->completed()->create();
+
+        Review::factory()->forBooking($booking)->create();
     }
 }
