d465d71f687e9a558a38933914bbb6037cc9d3a8
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-09-15 12:09:46 +0200

    feat: add auth register route

--- ServiceHub/routes/api.php
+++ ServiceHub/routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::name("auth.")->controller(controller: "App\Http\Controllers\AuthController")->group(function () {
+    Route::post(uri: "auth/register", action: "register")->name(name: "register");
+});
