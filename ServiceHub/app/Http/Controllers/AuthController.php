2d6e11efeb908c1c31bc340df84e9308eef087aa
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-09-20 12:39:00 +0200

    feat: implement user login with token authentication

--- ServiceHub/app/Http/Controllers/AuthController.php
+++ ServiceHub/app/Http/Controllers/AuthController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Enums\Users\UserRole;
 use App\Enums\Users\UserStatus;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -29,5 +31,22 @@ class AuthController extends Controller
             "user" => $user,
             "token" => $token->plainTextToken,
         ], status: 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::where("email", $data["email"])->first();
+        if(!$user || !Hash::check($data["password"], $user->password)) {
+            return response()->json([
+                "message" => "The provided credentials are incorrect."
+            ], status: 401);
+        }
+
+        $token = $user->createToken("auth_token");
+        return response()->json([
+            "user" => $user,
+            "token" => $token->plainTextToken,
+        ]);
     }
 }
